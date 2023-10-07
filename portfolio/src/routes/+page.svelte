1810d272e206241a8acf59732db1fc21ee88f757
Héloïse P <heloise.pingitore@mmibordeaux.com>
2023-10-07 20:11:17 +0200

    Add Hearder

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <script>
+  import Headers from "./component/Headers.svelte";
+
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Héloïse Pingitore</title>
+	<meta name="description" content="Project" />
 </svelte:head>
+<Headers />
 
 <section>
 	<h1>Portfolio</h1>
+	<p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+		Mollitia unde, minus veritatis numquam a aperiam, rem sit culpa, 
+		sed ad iste porro asperiores 
+		nam expedita totam laborum distinctio facilis deleniti.</p>
+		
 </section>
 
 <style lang="scss">
---
+++ portfolio/src/routes/component/Headers.svelte
@@ -0,0 +1,56 @@
+<script>
+</script>
+
+<div class="blur"></div>
+<div class="header">
+    <h3>PINGITORE Héloïse</h3>
+    <div class="link">
+        <h4>
+            Project
+        </h4>
+        <h4>
+            Contact
+        </h4>
+        <h4>
+            Curriculum Vitae
+        </h4>
+    </div>
+</div>
+
+<style lang="scss" >
+    .header{
+        position: fixed;
+        top: 0;
+        width: 100vw;
+        height: 70px;
+        padding-left: 6.25rem;
+        padding-right: 6.25rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .link{
+            display: flex;
+            gap: 1rem;
+
+            
+        }
+    }
+
+    h4, h3{
+        text-wrap: nowrap;
+    }
+
+    .blur{
+        position: fixed;
+        top: 0;
+        width: 100vw;
+        height: 70px;
+        -webkit-filter: blur(10px);
+        -moz-filter: blur(10px);
+        -o-filter: blur(10px);
+        -ms-filter: blur(10px);
+        filter: blur(10px);
+        backdrop-filter: blur(10px);
+    }
+</style>
