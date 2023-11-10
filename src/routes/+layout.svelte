ddd728819c7ddd1566b6c54da51587a606843794
Héloïse P <heloise.pingitore@mmibordeaux.com>
2023-11-10 15:48:54 +0100

    Add badget

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,14 +15,19 @@
     <p>
       © 2023 Héloïse Pingitore - Tous droits réservés
     </p>
+    <div id="wcb" class="carbonbadge"></div>
+    <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
   </footer>
 </div>
 
 <style>
   footer{
+    padding: 1rem;
     border-top: 1px solid black;
-    height: 3rem;
+    height: fit-content;
+    gap: 1rem;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
   }
